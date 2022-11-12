2f18f92f498e04b5256bc6dc1c1938561c969a17
Harper <harperweigle@gmail.com>
2022-11-12 09:03:34 -0800

    started modularizing unit tests

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+#include "unit_tests.h"
+
+using namespace std;
+using namespace utillib;
+using namespace nodelib;
+
+int main(){
+  uint8_t test_num = 0;
+  status_t status;
+
+  return status;
+}
---
+++ tests/unit_tests.h
@@ -0,0 +1,5 @@
+#include "status.h"
+
+status_t utilityTests(void);
+status_t interactionTests(void);
+status_t deletionTests(void);
